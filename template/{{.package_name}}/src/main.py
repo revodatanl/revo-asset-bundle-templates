331cffda7cc20ee4c0fb9b3614ddf59428a7b2e6
Thomas Brouwer <brouwer.thomas@gmail.com>
2024-04-11 20:27:44 +0200

    Initial commit

---
+++ template/{{.package_name}}/src/main.py
@@ -0,0 +1,8 @@
+"""Main modules for the {{.package_name}} package."""
+
+def main() -> None:
+    """Main function for the {{.package_name}} package."""
+    return
+
+if __name__ == "__main__":
+    pass
