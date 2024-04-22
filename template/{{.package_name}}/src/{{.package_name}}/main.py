f4d04707ec3cac9d5d4a03e7763b0a6cb4acbca6
Thomas Brouwer <brouwer.thomas@gmail.com>
2024-04-22 20:58:29 +0200

    Update makefile configuration

--- template/{{.package_name}}/src/{{.package_name}}/main.py
+++
@@ -1,8 +0,0 @@
-"""Main modules for the {{.package_name}} package."""
-
-def main() -> None:
-    """Main function for the {{.package_name}} package."""
-    return
-
-if __name__ == "__main__":
-    pass
